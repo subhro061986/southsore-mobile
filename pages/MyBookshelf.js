19ff2feba82a75eafdd6658163fb3086baaf2026
anniechakraborty <the.demented.genius29@gmail.com>
2024-03-11 16:34:54 +0530

    bookshelf reader

--- pages/MyBookshelf.js
+++ pages/MyBookshelf.js
@@ -17,17 +17,19 @@ import {
     PermissionsAndroid
 } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
+import Overlay from 'react-native-modal-overlay';
 import FooterPub from '../Global/FooterPub.js';
 import TopBar from '../Global/TopBar.js';
 import Footer from '../Global/Footer.js';
 import { useAuth } from '../Context/Authcontext.js';
 import { UserProfile } from '../Context/Usercontext.js';
-import Config from "../config/Config.json"
+import Config from "../config/Config.json";
+
 export const MyBookshelf = ({ navigation }) => {
 
     const { authData } = useAuth()
     const { getBookShelf, myBookList } = UserProfile()
-    // const [myBooks, setMyBooks] = useState([])
+    const [bookReading, setBookReading] = useState('')
     const [bookReadType, setBookReadType] = useState(0)
     const [readerModalvisibility, setReaderModalvisibility] = useState(false);
     useEffect(() => {
@@ -37,10 +39,14 @@ export const MyBookshelf = ({ navigation }) => {
     const bookTypeValue = [
         {
             id: 0,
-            title: 'PDF'
+            title: 'Please select a type'
         },
         {
             id: 1,
+            title: 'PDF'
+        },
+        {
+            id: 2,
             title: 'EPUB'
         }
     ]
@@ -49,14 +55,32 @@ export const MyBookshelf = ({ navigation }) => {
         setBookReadType(itemValue)
         console.log("Item val", itemValue);
         console.log("Item index", itemIndex);
-        let book_type_val = itemValue
-        if (book_type_val === 0) {
-            // setLowToHigh();
+        let book_type_val = itemValue;
+        setReaderModalvisibility(false);
+        if (book_type_val === 1) {
+            // open pdf book
+            if(bookReading !== null){
+                navigation.navigate('pdf', { epdf: Config.API_URL + Config.PUB_IMAGES + bookReading.publisherid + "/" + bookReading.epdf_link })
+            }
+        }
+        else if (book_type_val === 2) {
+            // open epub book
+            if(bookReading !== null){
+                navigation.navigate('pdf', { epdf: Config.API_URL + Config.PUB_IMAGES + bookReading.publisherid + "/" + bookReading.epub_link })
+            }
         }
         else {
-            // setHightoLow();
+            console.log("Selection failed")
         }
 
+    }
+
+    const readerModalHandler = () => {
+        setReaderModalvisibility(true);
+    }
+
+    const backbuttonhandler = () => {
+        setReaderModalvisibility(false);
     }
 
     // const navigateToReadScreen = async (book) => {
@@ -109,23 +133,29 @@ export const MyBookshelf = ({ navigation }) => {
     //             )
     //     }
     // }
+
     const getExt = (url) => {
         // navigation.navigate("/pdf")
         let text = "How are you doing today?";
         const myArray = text.split(" ");
     }
 
-    const navigateToReadScreen = async(book)=> {
-        if(book.epdf_link!== null || book.epdf_link!== 'null' && book.epub_link !== null || book.epub_link !== 'null'){
-            setReaderModalvisibility(true)
+    const navigateToReadScreen = async (book) => {
+        console.log("Reading : ", book);
+        if ((book.epdf_link !== null && book.epdf_link !== 'null') && (book.epub_link !== null && book.epub_link !== 'null')) {
+            console.log("Inside both");
+            setReaderModalvisibility(true);
+            setBookReading(book);
         }
-        else if(book.epdf_link !== null || book.epdf_link !== 'null'){
-
+        else if (book.epdf_link !== null || book.epdf_link !== 'null') {
+            console.log("Inside pdf");
+            navigation.navigate('pdf', { epdf: Config.API_URL + Config.PUB_IMAGES + book.publisherid + "/" + book.epdf_link })
         }
-        else if(book.epub_link !== null || book.epub_link !== 'null'){
-
+        else if (book.epub_link !== null || book.epub_link !== 'null') {
+            console.log("Inside epub");
+            navigation.navigate('pdf', { epdf: Config.API_URL + Config.PUB_IMAGES + book.publisherid + "/" + book.epub_link })
         }
-        else{
+        else {
 
         }
     }
@@ -142,9 +172,10 @@ export const MyBookshelf = ({ navigation }) => {
                         {myBookList.length} items
                     </Text>
                 </View>
-                <View style={[xStyle.cartPageBooksMainDiv, {
-                    marginBottom: '50%'
-                }]}>
+                <View style={[
+                    xStyle.cartPageBooksMainDiv,
+                    { marginBottom: '50%' }
+                ]}>
                     {
                         myBookList.length > 0 && myBookList.map((book, index) => (
                             <View style={[xStyle.pub_home_best_card]} key={index}>
@@ -170,10 +201,10 @@ export const MyBookshelf = ({ navigation }) => {
                                             </View>
                                         </View>
                                         {/* <TouchableOpacity>
-                                    <Image
-                                        source={require('../assets/images/greenTick.png')}
-                                    />
-                                </TouchableOpacity> */}
+                                            <Image
+                                                source={require('../assets/images/greenTick.png')}
+                                            />
+                                        </TouchableOpacity> */}
                                     </View>
                                     <View style={{
                                         display: 'flex',
@@ -190,7 +221,6 @@ export const MyBookshelf = ({ navigation }) => {
                                                 Read Now
                                             </Text>
                                         </TouchableOpacity>
-
                                         {/* <TouchableOpacity
                                             style={[xStyle.wishlistMoveToCartBtn, { width: '50%' }]}
                                             onPress={() => navigation.navigate('epub')}
@@ -199,40 +229,77 @@ export const MyBookshelf = ({ navigation }) => {
                                                 Epub
                                             </Text>
                                         </TouchableOpacity> */}
-                                        <View style={{
-                                            width: '48%',
-                                            borderWidth: 1,
-                                            borderColor: 'grey',
-                                            height: 35
-                                        }}>
-                                            <Picker
-                                                style={{
-                                                    fontSize: 12,
-                                                    color: 'grey',
-                                                }}
-                                                selectedValue={bookReadType}
-                                                onValueChange={
-                                                    (itemValue, itemIndex) =>
-                                                        handleBookReadType(itemValue, itemIndex)
-                                                }
-                                            >
-                                                {
-                                                    bookTypeValue.map((data, index) => (
-                                                        <Picker.Item label={data.title} value={data.id} key={index} />
-                                                    ))
-                                                }
-                                            </Picker>
-                                        </View>
                                     </View>
                                 </View>
                             </View>
-                        ))}
-
-
+                        ))
+                    }
                 </View>
-
             </ScrollView>
             <Footer />
+            <View>
+                <Overlay
+                    // animationType={ZoomIn}
+                    // transparent={true}
+                    visible={readerModalvisibility}
+                    // // isVisible={modalvisibility}
+                    // onRequestClose={backbuttonhandler}
+                    // hasBackdrop={true}
+                    // backdropColor={'black'}
+                    // // statusBarTranslucent={true}
+                    // backdropOpacity={0.5}
+                    onClose={backbuttonhandler}
+                    closeOnTouchOutside
+                    containerStyle={{ backgroundColor: 'rgba(38, 37, 37, 0.78)' }}
+                    childrenWrapperStyle={{ backgroundColor: '#FFFFFF', borderRadius: 30 }}
+                >
+                    <TouchableOpacity
+                        style={xStyle.buy_join_modal_cross}
+                        onPress={backbuttonhandler}
+                    >
+                        <Image
+                            source={require('../assets/images/close-circle.png')}
+                        />
+                    </TouchableOpacity>
+                    <View style={xStyle.buy_join_modal_head_view}>
+                        <Text style={xStyle.buy_join_modal_head}>Select Book Type</Text>
+                    </View>
+
+                    <View style={xStyle.logInModalBody}>
+                        <View style={{
+                            width: 200,
+                            borderWidth: 1,
+                            borderColor: 'grey',
+                            borderRadius: 20,
+                            // height: 40,
+                            // padding: 10,
+                        }}>
+                            <Picker
+                                style={{
+                                    fontSize: 12,
+                                    color: 'grey'
+                                }}
+                                selectedValue={bookReadType}
+                                onValueChange={
+                                    (itemValue, itemIndex) =>
+                                        handleBookReadType(itemValue, itemIndex)
+                                }
+                            >
+                                {
+                                    bookTypeValue.map((data, index) => (
+                                        <Picker.Item label={data.title} value={data.id} key={index} />
+                                    ))
+                                }
+                            </Picker>
+                        </View>
+                    </View>
+                    {/* <TouchableOpacity style={xStyle.logInBtn}
+                    // onPress={sendEmail}
+                    >
+                        <Text style={[xStyle.logInBtnText]}>Send Request</Text>
+                    </TouchableOpacity> */}
+                </Overlay>
+            </View>
         </SafeAreaView>
     )
 }
